192f43409bf7af2fff6892fa738f87dfbeae86f1
Vootla Chaitanya <speedblaze@gmail.com>
2019-08-28 14:56:08 +0530

    Update echoTest.sh

--- echoTest.sh
+++ echoTest.sh
@@ -4,5 +4,7 @@ while [[ $c -le 30 ]]
 do 
   # your-unix-command-here
    echo "Echoing $c"
+   echo "The Bash is a generic step type that enables executing any shell command. This general-purpose step can be used to execute any action that can be scripted, even with tools and services that haven't been integrated with JFrog Pipelines. This is the most versatile of the steps while taking full advantage of what the lifecycle offers.
+    All native steps derive from the Bash step. This means that all steps share the same base set of tags from Bash, while native steps have their own additional tags as well that support the step's particular function. So it's important to be familiar with the Bash step definition, since it's the core of the definition of all other steps."
    let c=c+1
 done
